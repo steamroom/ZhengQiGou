7c05436e60ea2298338db0f2a237868598f09883
unknown <1364863807@qq.com>
2023-07-17 15:12:24 +0800

    element主题色定制

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,46 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+//ElementPlus 按需引入
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+
+// 导入对应包
+import ElementPlus from "unplugin-element-plus/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      //配置elemrntPlus采用sass样式配色系统
+      resolvers: [ElementPlusResolver({importStyle:'sass'})],
+    }),
+    // 按需定制主题配置
+    ElementPlus({
+      useSource: true,
+    }),
   ],
   resolve: {
     alias: {
       //实际的路径转换“@”转成src
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      },
+    },
+  },
+});
