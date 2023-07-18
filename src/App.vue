6d48c9697b55fe9d881cf61c18eefdb7ed44ecfe
unknown <1364863807@qq.com>
2023-07-18 18:07:20 +0800

    scss文件自动导入

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,12 @@ import { } from 'vue-router'
 <template>
 <!-- 一级路由的出口组件 -->
 <router-view></router-view>
+<div class="testvar"> test var scss</div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.testvar{
+    color: $xtxColor;
+}
 </style>
 
