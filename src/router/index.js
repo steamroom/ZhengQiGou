42c1a2fda5d13bebb134768d8d8e4fd4c7d699d5
steamroom <1364863807@qq.com>
2023-07-25 17:12:56 +0800

    二级分类-路由切换优化

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import Layout from "@/views/Layout/index.vue";
 import Login from "@/views/Login/index.vue";
 import Home from "@/views/Home/index.vue";
 import Category from "@/views/Category/index.vue";
-import SubCategory from "@/views/SubCategory/index.vue"
+import SubCategory from "@/views/SubCategory/index.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -36,6 +36,12 @@ const router = createRouter({
       component: Login,
     },
   ],
+  //路由切换时候回到顶部
+  scrollBehavior() {
+    return {
+      top: 0,
+    };
+  },
 });
 
 export default router;
