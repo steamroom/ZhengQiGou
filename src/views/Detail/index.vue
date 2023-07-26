620e5f581e9a61902704ef9e860bf73d0abc5adc
steamroom <1364863807@qq.com>
2023-07-26 17:56:48 +0800

    详情-放大镜效果

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -21,7 +21,7 @@
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-
+              <ImageView></ImageView>
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>
@@ -126,6 +126,7 @@ import { getDetail } from "@/apis/detail";
 import { onMounted, ref } from "vue";
 import { useRoute } from "vue-router";
 import DetailHot from "./components/DetailHot.vue";
+import ImageView from "@/components/ImageView/index.vue";
 
 const goods = ref({});
 const route = useRoute();
